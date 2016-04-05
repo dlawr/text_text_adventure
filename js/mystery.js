2d6c1bcb2315d72955a2c09e89df1b346b433f65
dlawr <dan.lawrence3@gmail.com>
2016-04-05 12:21:30 -0400

    refactored to use req instead of global obj

--- js/mystery.js
+++ js/mystery.js
@@ -7,7 +7,7 @@ var gameState = {
 }
 
 function start(req, res, next) {
-  switch (gameState.location.split('-')[0]) {
+  switch (req.gameState.location.split('-')[0]) {
     case intro:
       intro();
       break;
@@ -17,11 +17,10 @@ function start(req, res, next) {
 }
 
 function intro(req, res, next) {
-  if(gameState.location.split('-').length === 1) {
-    gameState.location = 'intro';
-    gameState.text = `It is 9:30 pm on a Tuesday.  You are in for the night, watching tv.  You are thinking about going to bed, but you don’t really feel like getting off the couch.  A particularly irritating commercial is interrupted by the ringing of your phone.  You glance at the caller ID.  It’s the precinct.`
+  if(req.gameState.location.split('-').length === 1) {
+    req.gameState.location = 'intro';
+    req.gameState.text = `It is 9:30 pm on a Tuesday.  You are in for the night, watching tv.  You are thinking about going to bed, but you don’t really feel like getting off the couch.  A particularly irritating commercial is interrupted by the ringing of your phone.  You glance at the caller ID.  It’s the precinct.`
 
   }
-  req.gameState = gameState;
   next();
 }
