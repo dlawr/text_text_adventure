edbfa528a2e1a894a122257d23e6185737789b5a
dlawr <dan.lawrence3@gmail.com>
2016-04-05 18:17:12 -0400

    put user save data where I want it

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,17 @@
-function setGameState() {
+var save = require('../db/pgp/save');
 
+function setGameState(req, res, next) {
+  req.gameState = {};
+  req.gameState.phone = req.body.From;
+  req.gameState.location = res.save.location;
+  req.gameState.text = res.save.flavor_text;
+  req.gameState.choices = res.save.choices;
+  req.gameState.input = req.body.Body;
+  next()
 }
 
 function checkInput() {
 
 }
+
+module.exports.setGameState = setGameState;
