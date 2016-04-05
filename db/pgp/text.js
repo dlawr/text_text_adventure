9acbe521c9c7a39047ab4adf0141f5cb1fb49ec1
dlawr <dan.lawrence3@gmail.com>
2016-04-05 09:35:31 -0400

    apparently I cant assign consts in if statements

--- db/pgp/text.js
+++ db/pgp/text.js
@@ -1,17 +1,18 @@
 'use strict';
 const pgp = require('pg-promise')({});
 
-if (process.env.ENVIRONMENT === 'dev') {
-  const cn = {
+if (process.env.ENVIRONMENT === 'production') {
+  var cn = process.env.DATABASE_URL;
+} else {
+  var cn = {
     host: process.env.HOST, // server name or IP address;
     port: 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS
   }
-} else if (process.env.ENVIRONMENT === 'production') {
-  const cn = process.env.DATABASE_URL;
 }
+
 
 const db = pgp(cn);
 
