690f2ed669091f39998ba893c4effb7535024922
dlawr <dan.lawrence3@gmail.com>
2016-04-05 13:34:27 -0400

    conditional for first choice

--- js/mystery.js
+++ js/mystery.js
@@ -3,7 +3,9 @@
 var gameState = {
   phone: 0,
   location: '',
-  text: ''
+  text: '',
+  choices = [],
+  input: = ''
 }
 
 function start(req, res, next) {
@@ -18,9 +20,15 @@ function start(req, res, next) {
 
 function intro(req, res, next) {
   if(req.gameState.location.split('-').length === 1) {
-    req.gameState.location = 'intro';
-    req.gameState.text = `It is 9:30 pm on a Tuesday.  You are in for the night, watching tv.  You are thinking about going to bed, but you don’t really feel like getting off the couch.  A particularly irritating commercial is interrupted by the ringing of your phone.  You glance at the caller ID.  It’s the precinct.`
+    req.gameState.location = 'intro-c1';
+    req.gameState.text = `It is 9:30 pm on a Tuesday.  You are in for the night, watching tv.  You are thinking about going to bed, but you don’t really feel like getting off the couch.  A particularly irritating commercial is interrupted by the ringing of your phone.  You glance at the caller ID.  It’s the precinct.
 
+    Do you pick it up?
+    Text
+    yes or no`;
+    req.gameState.choices = ['yes', 'no'];
+  } else {
+    if(input === 'yes')
   }
   next();
 }
