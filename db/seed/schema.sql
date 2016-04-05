c128c37c20895ac70e83ab5c449c708682065999
dlawr <dan.lawrence3@gmail.com>
2016-04-05 17:42:44 -0400

    schema to save users game state

--- db/seed/schema.sql
+++ db/seed/schema.sql
@@ -4,3 +4,11 @@ create table texts(
   text_id serial primary key unique,
   body jsonb
 );
+
+create table states(
+  state_id serial primary key unique,
+  phone text,
+  location text,
+  flavor_text text,
+  choices text[]
+)
