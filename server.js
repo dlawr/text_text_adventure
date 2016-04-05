17a85f101b1aa016b82ee72a52d58b8d5d5416a0
dlawr <dan.lawrence3@gmail.com>
2016-04-05 18:20:02 -0400

    soon the game route will have ALL the functions kekekeke

--- server.js
+++ server.js
@@ -45,10 +45,11 @@ app.get('/messages', (req, res) => {
 })
 const text = require('./db/pgp/text.js');
 const save = require('./db/pgp/save.js');
+const setup = require('./js/setup.js');
 app.post('/text', text.newText, (req, res) => {res.json(res.text_id)})
 app.get('/text', text.allTexts, (req, res) => {res.json(res.texts)})
 
-app.post('/game',  (req, res) => {
+app.post('/game', save.getSaveData, setup.setGameState, (req, res) => {
   // client.messages.create({
   //   to: `+${req.params.number}`,
   //   from: `+${process.env.PHONE}`,
