8f1ed365bbb1fce8924e2fd8615d0f59bd516db6
dlawr <dan.lawrence3@gmail.com>
2016-04-07 09:51:33 -0400

    set user input to lower case

--- js/setup.js
+++ js/setup.js
@@ -2,9 +2,9 @@ var save = require('../db/pgp/save');
 
 function setGameState(req, res, next) {
   req.gameState = {};
-  req.gameState.phone = req.body.From;
+  req.gameState.phone = req.body.From.toLowerCase();
   req.gameState.location = res.save.location;
-  req.gameState.input = req.body.Body;
+  req.gameState.input = req.body.Body.toLowerCase();
   next()
 }
 
