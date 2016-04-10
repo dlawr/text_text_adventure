c7a457e5ee4e7bec189634565a97e0ef91eda975
dlawr <dan.lawrence3@gmail.com>
2016-04-10 12:59:34 -0400

    removed duplicate instructions

--- js/mystery.js
+++ js/mystery.js
@@ -43,8 +43,7 @@ function see(req, person) {
 function start(req, res, next) {
   if (badInput(req)) {
     console.log(req.gameState);
-    req.gameState.text = `What was that? your choices are:
-${req.gameState.choices.join(' | ')}`
+    req.gameState.text = `What was that? your choices are:`
     next();
   } else {
     directory(req, res, next)
@@ -88,31 +87,22 @@ function intro(req, res, next) {
     req.gameState.location = 'intro-c1';
     req.gameState.text = `It is 9:30 pm on a Tuesday.  You are in for the night, watching tv.  You are thinking about going to bed, but you don’t really feel like getting off the couch.  A particularly irritating commercial is interrupted by the ringing of your phone.  You glance at the caller ID.  It’s the precinct.
 
-    Do you pick it up?
-    Text
-    yes or no`;
+    Do you pick it up?`;
     req.gameState.choices = ['yes', 'no'];
   } else if(location[1] === 'c1'){
     if(req.gameState.input === 'yes') {
       req.gameState.location = 'intro-end';
-      req.gameState.text = `You groan, and answer it.  “What is it now?”  “Quit with the attitude, detective.” the sergeant snaps.  “There’s been a murder.”
-
-      Text
-      drive`;
+      req.gameState.text = `You groan, and answer it.  “What is it now?”  “Quit with the attitude, detective.” the sergeant snaps.  “There’s been a murder.”`;
       req.gameState.choices = ['drive'];
     } else {
-      req.gameState.text = `The phone continues to ring, do you pick it up?
-
-      Text
-      yes or no`
+      req.gameState.text = `The phone continues to ring, do you pick it up?`;
       req.gameState.choices = ['yes', 'no'];
     }
   } else {
     req.gameState.location = 'house';
     req.gameState.text = `You arrive at the crime scene by 10:00, which was frankly a miracle given the traffic.  The uniform on scene briefs you on your way in.  “The victim was found in the dining room.” he says. “He’s been ID’d as Daniel Jenkins, lives at 34th and Lex.  He was here for a dinner party.  There were 9 other guests.  The hosts, Bert and Victoria Eichmann, are in the kitchen.  There’s another couple, the Gerholds, in the living room with a Ms. Cremin.  Mr. Cremin is in the rec room with John Gardner and his wife,  Dr. Serenity Schaden.  The victim’s wife, Diana Jenkins, is in the office.
 
-    Where would you like to go first?”
-    office, rec room, kitchen, or living room`;
+    Where would you like to go first?”`;
     req.gameState.choices = ['office', 'rec room', 'kitchen', 'living room'];
   }
   next();
@@ -139,9 +129,7 @@ function house(req, res, next) {
 
 function office(req, res, next) {
   req.gameState.choices = ['office', 'rec room', 'kitchen', 'living room'];
-  req.gameState.text = `The office is fairly small, with a wooden desk and bookshelves, and a pair of leather armchairs on either side of a small table.  The table holds the end of a chess game.  There are few pieces left, and the white king is in checkmate near one corner of the board.  A tall blonde woman is pacing nervously.  You approach.  “Mrs. Jenkins, I’m so sorry for your loss.  Please, have a seat.  What can you tell me about what happened tonight?”  “After dinner, we all split up.  I came in here with Serenity for a game of chess.  We had a lot to catch up on, we were roommates in college, but she and her husband only just moved back into town.  Anyway, we finished our game around 9:00 and she left to find her husband.  That’s all I know.”
-
-  office, rec room, kitchen, or living room`
+  req.gameState.text = `The office is fairly small, with a wooden desk and bookshelves, and a pair of leather armchairs on either side of a small table.  The table holds the end of a chess game.  There are few pieces left, and the white king is in checkmate near one corner of the board.  A tall blonde woman is pacing nervously.  You approach.  “Mrs. Jenkins, I’m so sorry for your loss.  Please, have a seat.  What can you tell me about what happened tonight?”  “After dinner, we all split up.  I came in here with Serenity for a game of chess.  We had a lot to catch up on, we were roommates in college, but she and her husband only just moved back into town.  Anyway, we finished our game around 9:00 and she left to find her husband.  That’s all I know.”`;
   see(req, 'Diana Jenkins');
   next();
 }
@@ -152,6 +140,7 @@ function recRoom(req, res, next) {
     req.gameState.text = `As you enter the rec room, you see a pool table and a foosball table off to the right, with what appears to be a pool game abandoned mid-stream.  On the left, three people are sitting on an overstuffed couch, in front of a television screen.  The TV is currently off.  Two of the people, a petite brunette and a man of middling height, are sitting close together at one end of the couch, while a balding man sits a few feet away, facing them.  You say, “I’d like to speak to …`
     req.gameState.choices = ['Dr. Serenity Schaden', 'John Gardner', 'Jacques Cremin']
   }
+  next();
 }
 
 module.exports = start;
